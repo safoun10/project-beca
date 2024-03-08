dc57361a0b06ac46cc98d0fbce0bb8596735a1b0
Safoun <safoun.official@gmail.com>
2024-03-08 19:09:20 +0600

    just one part done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Banner from "./components/banner/Banner";
 function App() {
 	return (
 		<div>
-			<div className="mx-2">
+			<div className="bg-color-b1">
 				<Banner></Banner>
 			</div>
 		</div>
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,8 +5,24 @@ const Banner = () => {
 	return (
 		<div className="mx-auto banner-bg">
 			<div className="px-50 row align-items-center justify-content-center ">
-				<div className="col-11 col-md-6 mx-auto">text</div>
-				<div className="col-11 col-md-6 mx-auto">photo</div>
+				<div className="col-11 col-md-6 mx-auto">
+					<div className="edition text-cursive">2024 EDITION</div>
+					<div className="display-5 text-cursive main-line">
+						Designing Digital <br /> Books for kids
+					</div>
+					<div className="my-3 sub-title">
+						Transforming Traditional Learning with Magnetic Puzzles,
+						Interactive Writing, and Animated Pronunciations
+					</div>
+					<div className="btn-custom mt-3 fs-6">Learn More</div>
+				</div>
+				<div className="col-11 col-md-6 mx-auto">
+					<img
+						className="img-fluid gm"
+						src="https://i.postimg.cc/nhpCcSQp/Whats-App-Image-2024-03-08-at-17-05-46-90f52dd4.jpg"
+						alt="beca logo"
+					/>
+				</div>
 			</div>
 		</div>
 	);
