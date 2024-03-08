e267cdf108f51714329c50b402c28b5a15dde0a7
Safoun <safoun.official@gmail.com>
2024-03-08 23:49:28 +0600

    added contact section with responsiveness

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "../aboutUs/AboutUs.css";
+import { IoMailOpenOutline } from "react-icons/io5";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+import { FiYoutube } from "react-icons/fi";
+import { TbBrandShopee } from "react-icons/tb";
+import { FaPhone } from "react-icons/fa6";
 
 const AboutUs = () => {
 	return (
@@ -7,7 +14,7 @@ const AboutUs = () => {
 			<div className="text-center display-5 text-cursive mb-3 ">
 				FIND <span className="bg-body-secondary px-2">US</span> ON
 			</div>
-			<div className="row justify-content-center align-items-center w-100">
+			<div className="row justify-content-center align-items-center w-100 mx-auto ">
 				<div className="col-11 col-md-4 mx-auto">
 					<img
 						src="https://i.ibb.co/5rnNy9x/download.jpg"
@@ -16,9 +23,106 @@ const AboutUs = () => {
 					/>
 				</div>
 				<div className="col-11 col-md-8 mx-auto">
-					<div className="row gap-3">
-						<a href="" className="text-decoration-none text-dark ">
-							<div className="col-5 social-card">Email</div>
+					<div className="row gap-3 w-100 ">
+						<a
+							href="mailto:bukueksplorasicalistunganak@gmail.com"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-3 d-flex justify-content-center align-items-center ">
+									<IoMailOpenOutline></IoMailOpenOutline>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Email</div>
+							</div>
+						</a>
+						<a
+							href="https://www.facebook.com/profile.php?id=61557076863822&mibextid=ZbWKwL"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center w-100">
+								<div className="fs-3 d-flex justify-content-center align-items-center ">
+									<RiFacebookCircleLine></RiFacebookCircleLine>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Facebook </div>
+							</div>
+						</a>
+						<a
+							href="http://instagram.com/beca.journey_"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-4 d-flex justify-content-center align-items-center ">
+									<FaInstagram></FaInstagram>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Instagram </div>
+							</div>
+						</a>
+						<a
+							href="https://tiktok.com/@beca.journey"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-4 d-flex justify-content-center align-items-center ">
+									<FaTiktok></FaTiktok>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Tiktok </div>
+							</div>
+						</a>
+						<a
+							href="https://www.youtube.com/@Beca.Journey"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-3 d-flex justify-content-center align-items-center ">
+									<FiYoutube></FiYoutube>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Youtube </div>
+							</div>
+						</a>
+						<a
+							href="https://shopee.co.id/beca.journey_"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-3 d-flex justify-content-center align-items-center ">
+									<TbBrandShopee></TbBrandShopee>{" "}
+								</div>
+								<div className="fs-5 ps-2"> shopee </div>
+							</div>
+						</a>
+						<a
+							href="http://www.tokopedia.com/beca-journey"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-2 d-flex justify-content-center align-items-center ">
+									<img
+										src="https://i.ibb.co/mJsPXvB/download.png"
+										alt=""
+										className="w-50"
+									/>
+								</div>
+								<div className="fs-5 ps-2"> TOKOPEDIA </div>
+							</div>
+						</a>
+						<a
+							href="http://www.tokopedia.com/beca-journey"
+							className="text-decoration-none text-dark col-5  me-3"
+							target="_blank"
+						>
+							<div className="social-card d-flex justify-content-center align-items-center ">
+								<div className="fs-5 d-flex justify-content-center align-items-center ">
+									<FaPhone></FaPhone>{" "}
+								</div>
+								<div className="fs-5 ps-2"> Phone </div>
+							</div>
 						</a>
 					</div>
 				</div>
